b281d7b8dc02d28a11567c0e213029b669093b49
Edialvesgon <eagoncalves@minha.fag.edu.br>
2026-07-08 20:50:40 -0300

    find all produtct

--- src/services/api.ts
+++ src/services/api.ts
@@ -30,7 +30,7 @@ async function request<T>(path: string, options?: RequestInit): Promise<T> {
 
 // Produtos
 export const produtoService = {
-  listar: () => request<ProdutoServidor[]>('/produto'),
+  listar: () => request<ProdutoServidor[]>('/produto/all'),
 
   // FormData não usa Content-Type: application/json, por isso fetch separado
   criar: (formData: FormData) =>
